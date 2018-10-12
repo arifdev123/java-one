83c0e4530a4e40cb45567097bc062d2d4ccaf33b
Arif <mhdarif.vdm@gmail.com>
2018-10-12 15:06:38 +0530

    Committer is changed. And another line of sysout statement is added in the code.

--- JavaTest.java
+++ JavaTest.java
@@ -1,5 +1,6 @@
 class JavaTest {
 	public static void main(String args[]) {
 		System.out.println("This is a sample simple java app for Git Basics");
+		System.out.println("This is second line of text");
 	}
 }
