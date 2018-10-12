ce59aa1e50e542bca15276ec4ccbdd124fda6539
Arif <mhdarif.vdm@gmail.com>
2018-10-12 15:25:55 +0530

    Third line of sysout is added.

--- JavaTest.java
+++ JavaTest.java
@@ -2,5 +2,6 @@ class JavaTest {
 	public static void main(String args[]) {
 		System.out.println("This is a sample simple java app for Git Basics");
 		System.out.println("This is second line of text");
+		System.out.println("This line of text is added after the testing branch");
 	}
 }
